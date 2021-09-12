db59c5f717de950081536b484bf372a06f9b63df
ahmed <ka.ahmed1@outlook.com>
2021-09-12 18:28:45 -0400

    commencé defit2A

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const exp
+const HTTP_PORTS = 3000;
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('<h1>My Node App</h1>')
+})
+
+
+app.listen(HTTP_PORTS,()=> {
+
+    console.log(`Serveur NodJS démérré sur http://localhost:${HTTP_PORTS}`)
+})
